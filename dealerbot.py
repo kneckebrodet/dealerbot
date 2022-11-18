26109c76fd0fc56a4e82a43818af73c82768d1e4
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2022-11-18 14:16:33 +0900

    Update dealerbot.py

--- dealerbot.py
+++ dealerbot.py
@@ -1,13 +1,18 @@
+##### IMPORT FLASK LIBRARY FOR WEBAPPLICATION #####
 from flask import Flask, render_template
+##### IMPORT RASPBERRY PI & TIME LIBRARY #####
 import RPi.GPIO as GPIO
 from time import sleep
 
+##### SETUP INPUT/OUTPUT PINS #####
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-freqWheel = 2000
-freqArm = 50
 
-#SETUP CATERPILLAR MOTOR PINS:
+##### VARIABLES TO CHANGE FREQUENCY FOR DC-MOTORS #####
+FREQWHEEL = 2000
+FREQARM = 50
+
+##### SETUP PINS FOR CATERPILLAR DUALMOTORS #####
 engPin = [5, 6, 20, 13]
 pwmPin = [19, 12]
 
@@ -20,13 +25,14 @@ PWM2 = GPIO.PWM(pwmPin[1], freqWheel)
 PWM1.start(0)
 PWM2.start(0)
 
+##### SET DIRECTION (WHEELS) & START POSITION VALUE #####
 GPIO.output(engPin[0], False)
 GPIO.output(engPin[1], True)
 GPIO.output(engPin[2], False)
 GPIO.output(engPin[3], True)
-currentposition = 0
+current_position = 0
 
-#SETUP PINS (CARD ARM)
+##### SETUP PINS FOR DEALERARM #####
 armDir1 = 17
 armDir2 = 27
 armPWM = 18
@@ -37,10 +43,12 @@ GPIO.setup(armPWM, GPIO.OUT)
 pwm = GPIO.PWM(armPWM, freqArm)
 pwm.start(0)
 
+##### SET DIRECTION (ARM) #####
 GPIO.output(armDir1, True)
 GPIO.output(armDir2, False)
 
-def setdirection(direction):
+##### DEFINE FUNCTION TO SET THE DIRECTION BEFORE MOVING #####
+def set_direction(direction):
     if direction == 1:
         GPIO.output(engPin[0], True)
         GPIO.output(engPin[1], False)
@@ -50,65 +58,64 @@ def setdirection(direction):
         GPIO.output(engPin[0], False)
         GPIO.output(engPin[1], True)
         GPIO.output(engPin[2], False)
-        GPIO.output(engPin[3], True)
-        
+        GPIO.output(engPin[3], True)     
     else:
         print("Valid direction wasn't set")
-    
 
+##### CREATE MOVE FUNCTION #####
 def move(currentLocation, location):
+    # If current location is less than target location: move forward
+    # If current location is bigger than target location: move backwards
     if currentLocation < location:
-        setdirection(1)
+        set_direction(1)
     else:
-        setdirection(0)
+        set_direction(0)
+        
+    # Get the transportation range by calculating the gap between current location and target
     location = abs(location - currentLocation)
-    """for i in range(1,101):
-        PWM1.ChangeDutyCycle(i)
-        PWM2.ChangeDutyCycle(i)"""
     PWM1.ChangeDutyCycle(100)
     PWM2.ChangeDutyCycle(100)
     sleep(location*2)
     PWM1.ChangeDutyCycle(0)
     PWM2.ChangeDutyCycle(0)
-    sleep(1)
-    
-def resetposition(currentposition):
-    setdirection(0)
-    for i in range(1,101):
-        PWM1.ChangeDutyCycle(i)
-        PWM2.ChangeDutyCycle(i)
+    sleep(0.5)
+
+##### CREATE RETURN TO STARTPOSITION FUNCTION #####
+def reset_position(currentposition):
+    set_direction(0)
+    PWM1.ChangeDutyCycle(100)
+    PWM2.ChangeDutyCycle(100)
     sleep(currentposition*2)
     PWM1.ChangeDutyCycle(0)
     PWM2.ChangeDutyCycle(0)
-    sleep(1)
+    sleep(0.5)
 
-#START FLASK WEBSERVER
+#START FLASK WEBAPPLICATION
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
 
-####"ifconfig + :5000/player/1/card/1/currentposition/0####
 @app.route("/player/<int:player>/card/<int:card>")
 def deal(player, card):
-    global currentposition
-        #RESET POSITION
+    global current_position
+    # IF RESET-BUTTON IS PRESSED(card=0): RETURN TO START POSITION
     if card == 0:
-        resetposition(currentposition)
+        reset_position(currentposition)
         currentposition = 0
-    #MOVE TO PLAYER    
-
+    #TAKE VALUES FROM PRESSED BUTTON, MOVE TO PLAYER
+    #AND DEAL CARDS
     else:
         move(currentposition, player)
-        currentposition = player
+        current_position = player
     
-    #DEAL CARDS
-    sleep(0.5)
-    pwm.ChangeDutyCycle(80)
-    sleep(0.2)
-    pwm.ChangeDutyCycle(0)
-    sleep(0.5)
+        # DEAL CARDS
+        sleep(0.5)
+        pwm.ChangeDutyCycle(80)
+        sleep(0.2)
+        pwm.ChangeDutyCycle(0)
+        sleep(0.5)
         
     return render_template("home.html")
         
